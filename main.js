5e03fc43bf58eefb7e1744abda0e39cfadbfe0e5
geto <uchihaclan243@gmail.com>
2023-12-13 09:39:20 +0700

    product-9-10

---
+++ main.js
@@ -0,0 +1,167 @@
+const products = [
+    {
+        id: 1,
+        name: "iphone",
+        price: 100000,
+        image: "./img/iphone 14.png",
+        category: "mobile",
+        description: "iphone 14"
+    },
+    {
+        id: 2,
+        name: "Hoddi",
+        price: 25.00,
+        image: "/img/clothe (1).jpg",
+        category: "clothes",
+        description: "Hoddi"
+    },
+    {
+        id: 3,
+        name: "Shose",
+        price: 15.00,
+        image: "./img/shose (1).jpg",
+        category: "shose",
+        description: "shose"
+    }
+    ,{
+        id: 4,
+        name: "hat",
+        price: 10.00,
+        image: "./img/shirt (1).jpg",
+        category: "shirt",
+        description: "shirt"
+    }
+    ,
+    {
+        id: 5,
+        name: "shirt",
+        price: 10.00,
+        image: "./img/shirt (3).jpg",
+        category: "shirt",
+        description: "shirt"
+    },
+    {
+        id : 6,
+        name : "pants",
+        price : 40.00,
+        image : "./img/pant (2).jpg",
+        category : "pants",
+        description : "pants"
+    }
+    ,
+    {
+        id : 7,
+        name : "shoes",
+        price : 20.00,
+        image : "./img/shose (4).jpg",
+        category : "shoes",
+        description : "shoes"
+    },
+    {
+        id : 8,
+        name : "headphone",
+        price : 60.00,
+        image : "./img/headphone (1).jpg",
+        category : "headphone",
+        description : "headphone"
+    },
+    {
+        id : 9,
+        name : "Iphone 14",
+        price : 100000,
+        image : "./img/iphone 14.png",
+        category : "Iphone",
+        description : "Iphone"
+    },
+    {
+        id : 10,
+        name : "Hoddi",
+        price : 25.00,
+        image : "/img/clothe (1).jpg",
+        category : "clothes",
+        description : "Hoddi"
+    },
+    {
+        id : 11,
+        name : "pants",
+        price : 40.00,
+        image : "./img/pant (2).jpg",
+        category : "pants",
+        description : "pants"
+    }
+    ,
+    {
+        id : 12,
+        name : "shoes",
+        price : 20.00,
+        image : "./img/shose (4).jpg",
+        category : "shoes",
+        description : "shoes"
+    },
+    {
+        id : 13,
+        name : "headphone",
+        price : 60.00,
+        image : "./img/headphone (1).jpg",
+        category : "headphone",
+        description : "headphone"
+    },
+    {
+        id : 14,
+        name : "Iphone 14",
+        price : 100000,
+        image : "./img/iphone 14.png",
+        category : "Iphone",
+        description : "Iphone"
+    },
+    {
+        id : 15,
+        name : "Hoddi",
+        price : 25.00,
+        image : "/img/clothe (1).jpg",
+        category : "clothes",
+        description : "Hoddi"
+    },
+    {
+        id : 16,
+        name : "pants",
+        price : 40.00,
+        image : "./img/pant (2).jpg",
+        category : "pants",
+        description : "pants"
+    }
+    ,
+    {
+        id : 17,
+        name : "shoes",
+        price : 20.00,
+        image : "./img/shose (4).jpg",
+        category : "shoes",
+        description : "shoes"
+    },
+    {
+        id : 18,
+        name : "headphone",
+        price : 60.00,
+        image : "./img/headphone (1).jpg",
+        category : "headphone",
+        description : "headphone"
+    },
+    {
+        id : 19,
+        name : "Iphone 14",
+        price : 100000,
+        image : "./img/iphone 14.png",
+        category : "Iphone",
+        description : "Iphone"
+    },
+    {
+        id : 20,
+        name : "Hoddi",
+        price : 25.00,
+        image : "/img/clothe (1).jpg",
+        category : "clothes",
+        description : "Hoddi"
+    }
+];
+
